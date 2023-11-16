9159ddc21b8ed426e42251e644c75884ecea27ad
DELL <DELL@DESKTOP-9R8A4IN>
2023-11-16 12:50:50 +0530

    eng3 third commit

---
+++ src/test/resources/TestCases/tc4Test.java
@@ -0,0 +1,13 @@
+package TestCases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc4Test {
+	@Test
+	public void m4()
+	{
+		Reporter.log("tc4 running", true);
+	}
+
+}
